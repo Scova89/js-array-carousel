973edeaf634dfd7dda6045215a925f81e47ee270
Francesco <scovascova@gmail.com>
2021-11-11 18:31:13 +0100

    array di titoli e testi

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Array di immagini
+const imagesArray = ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'];
+
+const titleArray = ['Svezia','Svizzera','Gran Bretagna','Germania','Paradise'];
+
+const textArray = ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+'Lorem ipsum',
+'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',]
+
+console.log(textArray);
